7254247294b640d688d2f73940041b6cc1a5fd59
Jessiek4413 <jessiek4413@gmail.com>
2016-01-20 16:44:18 -0800

    Initial Commit

---
+++ src/org/usfirst/frc/team3255/robot/subsystems/DriveTrain.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team3255.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+	TalonSRX = new frontLeftTalon; 
+	TalonSRX = new frontRightTalon;
+	
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
